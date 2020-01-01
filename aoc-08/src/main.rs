a19e2de49c11ad1d499b006efbe997c5f861b5f5
Emily Curry <emily@curry.sh>
2020-01-01 14:05:19 -0600

    [08] Part 2 Solution

--- aoc-08/src/main.rs
+++ aoc-08/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     let input = fs::read_to_string("aoc-08/input.txt").unwrap();
     let image = Image::from_string(WIDTH, HEIGHT, &input);
     validation(&image);
+    decode(&image);
+}
+
+fn decode(image: &Image) {
+    let output = image.to_string();
+    println!("{}", output);
 }
 
 fn validation(image: &Image) {
@@ -80,5 +86,46 @@ impl Image {
 
     pub fn count_digits(&self, digit: u32) -> Vec<u32> {
         self.layers.iter().map(|x| x.count_digit(digit)).collect()
+    }
+
+    pub fn to_string(&self) -> String {
+        let decoded = self.decode();
+        let mut s = String::new();
+        for y in &decoded.data {
+            for x in y {
+                match x {
+                    0 => s.push(' '),
+                    1 => s.push('█'),
+                    _ => panic!(),
+                }
+            }
+            if decoded.data.last().unwrap() != y {
+                s.push('\n');
+            }
+        }
+        s
+    }
+
+    fn decode(&self) -> ImageLayer {
+        let mut layer = ImageLayer::new();
+        for y in 0..self.height {
+            layer.data.push(Vec::new());
+            for x in 0..self.width {
+                layer.data[y as usize].push(self.px_at(x as usize, y as usize))
+            }
+        }
+        layer
+    }
+
+    fn px_at(&self, x: usize, y: usize) -> u32 {
+        for layer in &self.layers {
+            match layer.data[y][x] {
+                0 => return 0,
+                1 => return 1,
+                2 => continue,
+                _ => panic!("Unknown pixel color"),
+            }
+        }
+        panic!("All layers are transparent")
     }
 }
